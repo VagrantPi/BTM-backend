519b1fbde216fe9839494660b5c0582475fba3f8
VagrantPi <xup6u06m4@gmail.com>
2025-02-13 22:46:12 +0800

    feat: 新增 migration，在 新增 udx 到 btm_whitelists 表格

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -9,13 +9,15 @@ import (
 
 func main() {
 	db := db.ConnectToDatabase()
-	db.AutoMigrate(
+	if err := db.AutoMigrate(
 		&model.BTMUser{},
 		&model.BTMRole{},
 		&model.BTMWhitelist{},
 		&model.BTMLoginToken{},
 		&model.BTM_CIB{},
-	)
+	); err != nil {
+		panic(err)
+	}
 
 	// Initialize the repository
 	repo, err := di.NewRepo()
@@ -31,6 +33,12 @@ func main() {
 
 	// Initialize the admin
 	if err := repo.InitAdmin(tx); err != nil {
+		panic(err)
+	}
+
+	// migration
+	// 2025_02_13_新增 udx 到 btm_whitelists
+	if err := db.Exec("CREATE UNIQUE INDEX IF NOT EXISTS idx_btm_whitelist_address ON btm_whitelists (address);").Error; err != nil {
 		panic(err)
 	}
 }
